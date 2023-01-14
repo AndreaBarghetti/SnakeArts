cf5d204e2b7150b5b426256c182a14fdd309f0d2
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2023-01-14 01:15:34 +0100

    Update app.R

--- app.R
+++ app.R
@@ -3,39 +3,86 @@ library(shiny)
 
 source("R/snake_player.R")
 
-# space <- Space$new(30,30)
-# space$fill_space()
-
 ui <- fluidPage(
   
-    numericInput(inputId = "nrows",
-                 label = "Height",
-                 value = 10,
-                 min = 2, 
-                 max = 200),
+  sidebarLayout(sidebarPanel = sidebarPanel(
+    sliderInput(inputId = "nrows",
+                label = "Height",
+                value = 20,
+                min = 2, 
+                max = 150),
     
-    numericInput(inputId = "ncols",
-                 label = "Width",
-                 value = 10,
-                 min = 2, 
-                 max = 200),
+    sliderInput(inputId = "ncols",
+                label = "Width",
+                value = 20,
+                min = 2, 
+                max = 150),
     
-    actionButton("fill",label = "Fill"),
+    sliderInput(inputId = "linewidth",
+                label = "linewidth",
+                value = 3,
+                min = .1, 
+                max = 10),
     
-    plotOutput(outputId = "plot")
+    sliderInput(inputId = "dotsize",
+                label = "dotsize",
+                value = 3,
+                min = .1, 
+                max = 10),
+    
+    selectInput("palette",label = "Palatte",choices = LETTERS[1:8],selected = "A"),
+    
+    actionButton("move_head","Move Head"),
+    actionButton("new_snake","New Snake"),
+    actionButton("grow_snake","Grow Snake"),
+    actionButton("fill_space","Snake it all!"),
+    
+    actionButton("refresh","Refresh")
+  ),
+  mainPanel = mainPanel(
+    plotOutput(outputId = "plot",width = "1000px", height = "600px")
+  ))
+
 )
 
 server <- function(input, output) {
  
-  output$plot <- shiny::renderPlot({
-    
-    req(input$fill)
-    
+  space <- reactive({
+    req(input$nrows,input$ncols)
     space <- Space$new(input$nrows,input$ncols)
-    space$fill_space()
-    space$show()
+    space$new_snake()
   })
   
+  observeEvent(input$new_snake, {
+    space()$new_snake()
+  })
+  
+  observeEvent(input$grow_snake, {
+    space()$grow_snake()
+  })
+  
+  observeEvent(input$move_head, {
+    space()$move_head()
+  })
+  
+  observeEvent(input$fill_space, {
+    space()$fill_space()
+  })
+  
+  output$plot <- shiny::renderPlot({
+    
+    input$move_head
+    input$new_snake
+    input$grow_snake
+    input$fill_space
+    
+    space()$show(
+      linewidth=input$linewidth, 
+      background_color="black", 
+      border_color="white",
+      dotsize=input$dotsize
+    ) + scale_color_viridis_c(direction = 1, begin = 0, end = 1, option = input$palette)
+  })
 }
 
 shinyApp(ui, server)

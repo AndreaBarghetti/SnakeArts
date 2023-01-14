67b0aae26b7d7a7c6e9ab06a09b260df408d6e13
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2023-01-14 01:15:31 +0100

    Update example.R

--- example.R
+++ example.R
@@ -1,10 +1,27 @@
+library(tidyverse)
 source("R/snake_player.R")
 
 set.seed(1)
-space <- Space$new(50,90)
+space <- Space$new(80,150)
 space$fill_space()
+space$show() + snake_palette("A")
+
 get_space_stats(space)
-p <- space$show()
+
+space$show(linewidth = 1, dotsize=2) + snake_palette("D")
+
+p <- space$show(background_color="black",linewidth = 5, dotsize=4)
 p + scale_color_viridis_c(direction = 1, begin = 0.1, end = .9, option = "F")
 p + scale_color_viridis_c(direction = 1, begin = 0, end = 1, option = "D")
 # scale_color_manual(values = rep(c("white","red"),600))
+
+p1 <- p + scale_color_viridis_c(direction = 1, begin = 0.1, end = .9, option = "F")
+ggsave(p1, 
+       filename="image.svg", 
+       device = "svg", 
+       units = "cm", 
+       width = 150,
+       height = 80,
+       dpi = 320,
+       limitsize=F)
+
